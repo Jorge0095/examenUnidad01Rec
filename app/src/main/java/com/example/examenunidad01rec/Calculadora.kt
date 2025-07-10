79e7b4b2d0151b0b70a99e4696d03be0f40c9325
Jorge0095 <2022030095@upsin.edu.mx>
2025-07-09 17:16:03 -0700

    Se creo la clase de Kotlin "Calculadora".

---
+++ app/src/main/java/com/example/examenunidad01rec/Calculadora.kt
@@ -0,0 +1,22 @@
+package com.example.examenunidad01rec
+
+class Calculadora {
+    public var num1: Float = 0.0f
+    public var num2: Float = 0.0f
+
+    public fun suma(): Float {
+        return num1 + num2
+    }
+
+    public fun resta(): Float {
+        return num1 - num2
+    }
+
+    public fun multiplicacion(): Float {
+        return num1 * num2
+    }
+
+    public fun dividision(): Float {
+        return num1 / num2
+    }
+}
